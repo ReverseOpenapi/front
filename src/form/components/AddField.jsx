749fb0b1a43940fdef7f395e6d0efd6214ae855e
Tan B Nguyen Loreal (Capgemini) <tan.b.nguyen.loreal@capgemini.com>
2022-03-03 19:35:35 +0100

    :bug: Fix another bug and Add Tag Component

--- src/form/components/AddField.jsx
+++ src/form/components/AddField.jsx
@@ -1,29 +1,26 @@
-import React, { useState } from 'react'
-import '../../App.css'
+import React, { useState } from "react";
+import "../../App.css";
 
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import AddIcon from '@mui/icons-material/Add';
-import ListItem from '@mui/material/ListItem';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import ListItem from "@mui/material/ListItem";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
-
-export default function AddField({ addField, dataParentToChild}) {
-
+export default function AddField({ addField, dataParentToChild }) {
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  
   const [checked, setChecked] = React.useState([]);
 
   const handleToggle = (value) => () => {
@@ -37,33 +34,34 @@ export default function AddField({ addField, dataParentToChild}) {
     setChecked(newChecked);
   };
 
-  const OptionField = ["termsOfService", "contact", "license"]
+  const OptionField = ["termsOfService", "contact", "license"];
 
   //Delete data from OptionField
   for (let i = 0; i < dataParentToChild.length; i++) {
-    let dataCheck = OptionField.includes(dataParentToChild[i].key)
+    let dataCheck = OptionField.includes(dataParentToChild[i].key);
     if (dataCheck === true) {
       for (let j = 0; j < OptionField.length; j++) {
         if (OptionField[j] === dataParentToChild[i].key) {
-          OptionField.splice(j, 1)
-          console.log("SPLICE DATA")
-          console.log(OptionField)
-          // close the OptFieldList
-          // if (OptionField.length === 0) {
-          //   console.log("LAST IF")
-          //   return
-          // }
+          OptionField.splice(j, 1);
         }
       }
     }
   }
 
   return (
-    <div className='list-item'>
-      <Box className='App' sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', color: 'black' }}>
+    <div className="list-item">
+      <Box
+        className="App"
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          color: "black",
+        }}
+      >
         <nav aria-label="secondary mailbox folders">
           <List
-            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
             component="nav"
             aria-labelledby="nested-list-subheader"
           >
@@ -76,7 +74,14 @@ export default function AddField({ addField, dataParentToChild}) {
             </ListItemButton>
             <Collapse in={open} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                <List
+                  dense
+                  sx={{
+                    width: "100%",
+                    maxWidth: 360,
+                    bgcolor: "background.paper",
+                  }}
+                >
                   {OptionField.map((value) => {
                     const labelId = `checkbox-list-secondary-label-${value}`;
                     return (
@@ -87,7 +92,7 @@ export default function AddField({ addField, dataParentToChild}) {
                             edge="end"
                             onChange={handleToggle(value)}
                             checked={checked.indexOf(value) !== -1}
-                            inputProps={{ 'aria-labelledby': labelId }}
+                            inputProps={{ "aria-labelledby": labelId }}
                           />
                         }
                         disablePadding
@@ -99,12 +104,14 @@ export default function AddField({ addField, dataParentToChild}) {
                     );
                   })}
                 </List>
-                <Button primary onClick={() => addField(checked)}>Add Field</Button>
+                <Button primary onClick={() => addField(checked)}>
+                  Add Field
+                </Button>
               </List>
             </Collapse>
           </List>
         </nav>
       </Box>
     </div>
-  )
+  );
 }
--- src/form/components/Paths.jsx
+++ src/form/components/Paths.jsx
@@ -1,67 +1,96 @@
-import React, { useState, useEffect } from 'react'
-import "./style.css"
+import React, { useState, useEffect } from "react";
+import "./style.css";
 
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Chip from '@mui/material/Chip';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
 
 export default function Paths({ dataPath }) {
+  const operation = [
+    { key: "GET", color: "#3b83f6" },
+    { key: "PUT", color: "orange" },
+    { key: "POST", color: "#49cc90" },
+    { key: "DELETE", color: "red" },
+  ];
+  const [isClicked, setIsClicked] = useState(false);
+  const [opeSelected, setOpeSelected] = useState({ key: "", color: "" });
+  let path = "";
+  let dataComplete = {};
+  const back = false;
 
-    const operation = [{ key: "GET", color: "#3b83f6" }, { key: "PUT", color: "orange" }, { key: "POST", color: "#49cc90" }, { key: "DELETE", color: "red" }]
-    const [isClicked, setIsClicked] = useState(false)
-    const [opeSelected, setOpeSelected] = useState({ key: "", color: "" })
-    let path = ""
-    let dataComplete = {operationObj : "", path : ""}
+  //Select Operation
+  const chipsClick = (ope) => {
+    setOpeSelected(ope);
+    setIsClicked(true);
+  };
+  const selectOpe = () => {
+    setIsClicked(false);
+  };
 
-    //Select Operation
-    const chipsClick = (ope) => {
-        setOpeSelected(ope)
-        setIsClicked(true)
+  const handleChange = (event) => {
+    path = event.target.value;
+    //Need to selected operation to add DataPath
+    if (isClicked === false) {
+      dataComplete = {
+        operationObj: "",
+        path: "",
+        opeColor: "",
+      };
+    } else {
+      dataComplete = {
+        operationObj: opeSelected.key,
+        path: path,
+        opeColor: opeSelected.color,
+      };
     }
-    const selectOpe = () => {
-        setIsClicked(false)
-    }
+  };
 
-    const handleChange = (event) => {
-        path = event.target.value
-        dataComplete = {operationObj : opeSelected.key, path : path, opeColor : opeSelected.color}
-    }
+  const Field = (
+    <TextField
+      className="text-field"
+      name="test"
+      required
+      id="filled-required"
+      label="Path"
+      onChange={(e) => handleChange(e)}
+    />
+  );
 
-    const Field = (
-        <TextField
-            className="text-field"
-            name="test"
-            required
-            id="filled-required"
-            label="Path"
-            onChange={(e) => handleChange(e)}
-        />
-    )
-
-    return (
-        <div className="">
-            <div className="list-item" spacing={1}>
-                {isClicked ?
-                    <Button
-                        variant="contained"
-                        onClick={selectOpe}
-                        sx={{ backgroundColor: opeSelected.color }}
-                    >
-                        {opeSelected.key}
-                    </Button> :
-                    operation.map(ope => {
-                        return (
-                            <Chip label={ope.key} onClick={() => chipsClick(ope)}
-                                style={{ backgroundColor: ope.color, color: "white" }} />
-                        )
-                    }
-                    )
-                }
-            </div>
-            <div className="paths-field">
-            {Field}
-            <Button primary onClick={() => dataPath(dataComplete)}>Continue</Button>
-            </div>
-        </div>
-    )
+  //IF Operation is not selected, block the Add Path button
+  return (
+    <div className="">
+      <div className="list-item" spacing={1}>
+        {isClicked ? (
+          <Button
+            variant="contained"
+            onClick={selectOpe}
+            sx={{ backgroundColor: opeSelected.color, margin: "10px" }}
+          >
+            {opeSelected.key}
+          </Button>
+        ) : (
+          operation.map((ope) => {
+            return (
+              <Chip
+                label={ope.key}
+                onClick={() => chipsClick(ope)}
+                style={{
+                  backgroundColor: ope.color,
+                  color: "white",
+                  margin: "0px 5px 10px",
+                }}
+              />
+            );
+          })
+        )}
+      </div>
+      <div>{Field}</div>
+      <Button primary onClick={() => dataPath(back)}>
+        Back
+      </Button>
+      <Button primary onClick={() => dataPath(dataComplete)}>
+        Add Path
+      </Button>
+    </div>
+  );
 }
